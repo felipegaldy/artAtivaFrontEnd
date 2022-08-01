3c5b227f59fe35dc2cc88954eb18896fb350c3c8
Hericlis Ventura <hericlisventura.o@gmail.com>
2022-08-01 15:06:49 -0300

    adicionando componentes do react router-dom

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Box, FormHelperText, Input, InputLabel } from '@mui/material';
+import { FormControl } from '@material-ui/core';
 
 function Login() {
   return (
-    <div>Login</div>
+  <Box>
+    <Box>
+    <FormControl>
+  <InputLabel htmlFor="my-input">Email address</InputLabel>
+  <Input id="my-input" aria-describedby="my-helper-text" />
+  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+</FormControl>
+    </Box>
+  </Box>
   )
 }
 
