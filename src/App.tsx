4804d470dbd73c826288533f36c5ccdaf7d5832c
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-08-01 14:19:37 -0300

    home

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Navbar from './components/estaticos/navbar/Navbar';
 import TopNavbar from './components/estaticos/topnavbar/TopNavbar';
 import Footer from './components/estaticos/footer/Footer';
 import FooterNav from './components/estaticos/footernav/FooterNav';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <>
       <TopNavbar />
       <Navbar />
+      <Home />
       <FooterNav />
       <Footer />
     </>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Grid, Box, Typography, Button } from "@mui/material";
+import './Home.css';
+import { Paper } from "@material-ui/core";
+
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <Grid        
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center">
+        <Grid alignItems="center" item xs={12}>
+          <Box className="box1">
+            <img src="https://i.imgur.com/MRhYAHD.png" alt="" />
+          </Box>
+        </Grid>
+        <Box>
+            <Typography>
+              <h1>Quero levar para casa</h1>
+            </Typography>
+          </Box>
+        <Grid container spacing={3} className="container-produtos">
+        <Grid item xs={12} sm={3} className="card-produto">
+          <Box>
+          <img className="img-produto" src="https://images-ext-1.discordapp.net/external/IhO-E-_oJfwgGbJGyOsyRgxXfXZ6scXUnLUVsLkV-QE/https/i.imgur.com/336HGcz.png?width=627&height=621"  alt="" />
+          </Box>
+          <Box>
+            <Typography variant="h6">
+              Comprar
+              R$29,99
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={3} className="card-produto">
+          <Box>
+          <img className="img-produto" src="https://i.imgur.com/gUoTryU.jpeg" alt="Bracelete ancora"  />
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={3} className="card-produto">
+          <h1>ola mundo</h1>
+        </Grid>
+        <Grid item xs={12} sm={3} className="card-produto">
+          <h1>ola mundo</h1>
+        </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
-export default Home
+export default Home;
