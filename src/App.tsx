7410de2bd3811d425f7985a57a490cb0aed406a1
Matheus <matheuslsv7@gmail.com>
2022-08-04 13:26:48 -0300

    Criada a branch de backup do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/sobrenos" element={<SobreNos/>}/>
         <Route path="/cadastrousuario" element={<CadastroUsuario/>}/>
