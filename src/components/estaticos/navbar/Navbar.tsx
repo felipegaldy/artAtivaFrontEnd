a7796ffb73e2e06be2e0a1912971c2c1f4446115
Matheus <matheuslsv7@gmail.com>
2022-08-15 12:11:00 -0300

    Alterados elementos da tela de login e logo envia para pagina home

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -14,7 +14,7 @@ import "./Navbar.css";
 import { useDispatch, useSelector } from "react-redux";
 import { TokenState } from "../../../store/tokens/tokensReduce";
 import { addToken } from "../../../store/tokens/actions";
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -75,175 +75,177 @@ function Navbar() {
       autoClose: 2000,
       hideProgressBar: false,
       closeOnClick: true,
-      pauseOnHover:false,
+      pauseOnHover: false,
       draggable: false,
       theme: "colored",
       progress: undefined,
-  });
+    });
     navigate('/login')
-}
+  }
 
-var navbar;
+  var navbar;
 
-if(token === ""){
-   navbar = <Grid>
-    <AppBar position="static"  className="top-app-bar-container">
-   <Toolbar className="top-toolbar">
-     <Box className="whatsBox" >
-       <Button className="texto-whats botoes-top">
-         <WhatsAppIcon className="iconeWhats" /> Fale conosco via whatsapp
-       </Button>
-     </Box>
-     <Box className="botoesNav" >
-       <Link to="/login" className="text-decorator-none cursor">
-       <Button className="botoes-top" color="inherit">
-         Login
-       </Button>
-       </Link>{" "}
-       |{" "}
-       <Link to="/cadastrousuario" className="text-decorator-none cursor">
-       <Button className="botoes-top" color="inherit">
-         Cadastrar
-       </Button>
-       </Link>
-     </Box>
-   </Toolbar>
- </AppBar>
- {/* PARTE 2 DO HEADER COMEÇA AQUI*/}
- <AppBar position="static" className="app-bar-container" color="inherit">
-   <Toolbar className="header-toolbar">
-     <Typography
-       variant="h6"
-       noWrap
-       component="div"
-       sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-     >
-      <img src="https://images-ext-2.discordapp.net/external/zHKaw0ij_SdecgNwb3GbHXqvHyuYKxWvM7fkGCVmPw8/https/i.imgur.com/UGVgV8w.png" alt="" />
-     </Typography>
-     <Search className="search">
-       <SearchIconWrapper>
-         <SearchIcon />
-       </SearchIconWrapper>
-       <StyledInputBase
-         placeholder="Buscar..."
-         inputProps={{ "aria-label": "search" }}
-       />
-     </Search>
-     {/*icones de login e shopcart */}
-     <PersonIcon className="icones" />
-     <ShoppingCartIcon className="icones" />
-   </Toolbar>
-   <Box className="box-botoes-sobrenos-contato">
-       <Link to="/sobrenos" className="text-decorator-none cursor">
-       <Button className="botoes-top botoes-sobrenos-contato" variant="text" >
-         Quem somos
-       </Button>
-       </Link>{" "}
-       |{" "}
-       <Button className="botoes-top botoes-sobrenos-contato" variant="text">
-         Contato
-       </Button>
-     </Box>
-   <Box className="nav-botoes">
-     <Button variant="text" className="botao-nav">Roupas</Button>
-     <Button variant="text" className="botao-nav">Decoração</Button>
-     <Button variant="text" className="botao-nav">Destaques</Button>
-     <Button variant="text" className="botao-nav">Para Casa</Button>
-     <Button variant="text" className="botao-nav">Presentes</Button>
-   </Box>
- </AppBar>
- </Grid>
-}else {
-  navbar = 
-  <Grid>
+  if (token === "") {
+    navbar = <Grid>
+      <AppBar position="static" className="top-app-bar-container">
+        <Toolbar className="top-toolbar">
+          <Box className="whatsBox" >
+            <Button className="texto-whats botoes-top">
+              <WhatsAppIcon className="iconeWhats" /> Fale conosco via whatsapp
+            </Button>
+          </Box>
+          <Box className="botoesNav" >
+            <Link to="/login" className="text-decorator-none cursor">
+              <Button className="botoes-top" color="inherit">
+                Login
+              </Button>
+            </Link>{" "}
+            |{" "}
+            <Link to="/cadastrousuario" className="text-decorator-none cursor">
+              <Button className="botoes-top" color="inherit">
+                Cadastrar
+              </Button>
+            </Link>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {/* PARTE 2 DO HEADER COMEÇA AQUI*/}
+      <AppBar position="static" className="app-bar-container" color="inherit">
+        <Toolbar className="header-toolbar">
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            <Link to="/home">
+              <img src="https://images-ext-2.discordapp.net/external/zHKaw0ij_SdecgNwb3GbHXqvHyuYKxWvM7fkGCVmPw8/https/i.imgur.com/UGVgV8w.png" alt="Logo da empresa" />
+            </Link>
+          </Typography>
+          <Search className="search">
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Buscar..."
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          {/*icones de login e shopcart */}
+          <PersonIcon className="icones" />
+          <ShoppingCartIcon className="icones" />
+        </Toolbar>
+        <Box className="box-botoes-sobrenos-contato">
+          <Link to="/sobrenos" className="text-decorator-none cursor">
+            <Button className="botoes-top botoes-sobrenos-contato" variant="text" >
+              Quem somos
+            </Button>
+          </Link>{" "}
+          |{" "}
+          <Button className="botoes-top botoes-sobrenos-contato" variant="text">
+            Contato
+          </Button>
+        </Box>
+        <Box className="nav-botoes">
+          <Button variant="text" className="botao-nav">Roupas</Button>
+          <Button variant="text" className="botao-nav">Decoração</Button>
+          <Button variant="text" className="botao-nav">Destaques</Button>
+          <Button variant="text" className="botao-nav">Para Casa</Button>
+          <Button variant="text" className="botao-nav">Presentes</Button>
+        </Box>
+      </AppBar>
+    </Grid>
+  } else {
+    navbar =
+      <Grid>
 
-  <AppBar position="static"  className="top-app-bar-container">
-  <Toolbar className="top-toolbar">
-    <Box className="whatsBox">
-      <Button className="texto-whats botoes-top">
-        <WhatsAppIcon className="iconeWhats" /> Fale conosco via whatsapp
-      </Button>
-    </Box>
-    <Box className="botoesNav">      
-      <Link to="/cadastroprodutos" className="text-decorator-none cursor">
-      <Button className="botoes-top" color="inherit">
-       Cadastro produtos
-      </Button>
-      </Link>
-      |
-      {" "}
-      <Link to="/produtos" className="text-decorator-none cursor">
-      <Button className="botoes-top" color="inherit">
-       Produtos
-      </Button>
-      </Link>
-      |
-      {" "}
-      <Link to="/cadastrocategoria" className="text-decorator-none cursor">
-      <Button className="botoes-top" color="inherit">
-        Cadastrar categoria
-      </Button>
-      </Link>
-      |
-      <Link to="/categorias" className="text-decorator-none cursor">
-      <Button className="botoes-top" color="inherit">
-        Categoria
-      </Button>
-      </Link>
-      |
-      <Link to="/login" className="text-decorator-none cursor">
-      <Button onClick={goLogout} className="botoes-top" color="inherit">
-        Logout
-      </Button>
-      </Link>{" "}
-    </Box>
-  </Toolbar>
-</AppBar>
-{}
-<AppBar position="static" className="app-bar-container" color="inherit">
-  <Toolbar className="header-toolbar">
-    <Typography
-      variant="h6"
-      noWrap
-      component="div"
-      sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-    >
-      <img src="https://images-ext-2.discordapp.net/external/zHKaw0ij_SdecgNwb3GbHXqvHyuYKxWvM7fkGCVmPw8/https/i.imgur.com/UGVgV8w.png" alt="" />
-    </Typography>
-    <Search className="search">
-      <SearchIconWrapper>
-        <SearchIcon />
-      </SearchIconWrapper>
-      <StyledInputBase
-        placeholder="Buscar..."
-        inputProps={{ "aria-label": "search" }}
-      />
-    </Search>
-    {/*icones de login e shopcart */}
-    <PersonIcon className="icones" />
-    <ShoppingCartIcon className="icones" />
-  </Toolbar>
-  <Box className="box-botoes-sobrenos-contato">
-      <Link to="/sobrenos" className="text-decorator-none cursor">
-      <Button className="botoes-top botoes-sobrenos-contato" variant="text" >
-        Quem somos
-      </Button>
-      </Link>{" "}
-      |{" "}
-      <Button className="botoes-top botoes-sobrenos-contato" color="inherit">
-        Contato
-      </Button>
-    </Box>
-  <Box className="nav-botoes">
-    <Button variant="text" className="botao-nav">Roupas</Button>
-    <Button variant="text" className="botao-nav">Decoração</Button>
-    <Button variant="text" className="botao-nav">Destaques</Button>
-    <Button variant="text" className="botao-nav">Para Casa</Button>
-    <Button variant="text" className="botao-nav">Presentes</Button>
-  </Box>
-</AppBar>
-</Grid>
-}
+        <AppBar position="static" className="top-app-bar-container">
+          <Toolbar className="top-toolbar">
+            <Box className="whatsBox">
+              <Button className="texto-whats botoes-top">
+                <WhatsAppIcon className="iconeWhats" /> Fale conosco via whatsapp
+              </Button>
+            </Box>
+            <Box className="botoesNav">
+              <Link to="/cadastroprodutos" className="text-decorator-none cursor">
+                <Button className="botoes-top" color="inherit">
+                  Cadastro produtos
+                </Button>
+              </Link>
+              |
+              {" "}
+              <Link to="/produtos" className="text-decorator-none cursor">
+                <Button className="botoes-top" color="inherit">
+                  Produtos
+                </Button>
+              </Link>
+              |
+              {" "}
+              <Link to="/cadastrocategoria" className="text-decorator-none cursor">
+                <Button className="botoes-top" color="inherit">
+                  Cadastrar categoria
+                </Button>
+              </Link>
+              |
+              <Link to="/categorias" className="text-decorator-none cursor">
+                <Button className="botoes-top" color="inherit">
+                  Categoria
+                </Button>
+              </Link>
+              |
+              <Link to="/login" className="text-decorator-none cursor">
+                <Button onClick={goLogout} className="botoes-top" color="inherit">
+                  Logout
+                </Button>
+              </Link>{" "}
+            </Box>
+          </Toolbar>
+        </AppBar>
+        { }
+        <AppBar position="static" className="app-bar-container" color="inherit">
+          <Toolbar className="header-toolbar">
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            >
+              <img src="https://images-ext-2.discordapp.net/external/zHKaw0ij_SdecgNwb3GbHXqvHyuYKxWvM7fkGCVmPw8/https/i.imgur.com/UGVgV8w.png" alt="" />
+            </Typography>
+            <Search className="search">
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Buscar..."
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+            {/*icones de login e shopcart */}
+            <PersonIcon className="icones" />
+            <ShoppingCartIcon className="icones" />
+          </Toolbar>
+          <Box className="box-botoes-sobrenos-contato">
+            <Link to="/sobrenos" className="text-decorator-none cursor">
+              <Button className="botoes-top botoes-sobrenos-contato" variant="text" >
+                Quem somos
+              </Button>
+            </Link>{" "}
+            |{" "}
+            <Button className="botoes-top botoes-sobrenos-contato" color="inherit">
+              Contato
+            </Button>
+          </Box>
+          <Box className="nav-botoes">
+            <Button variant="text" className="botao-nav">Roupas</Button>
+            <Button variant="text" className="botao-nav">Decoração</Button>
+            <Button variant="text" className="botao-nav">Destaques</Button>
+            <Button variant="text" className="botao-nav">Para Casa</Button>
+            <Button variant="text" className="botao-nav">Presentes</Button>
+          </Box>
+        </AppBar>
+      </Grid>
+  }
 
   return (
     <>
