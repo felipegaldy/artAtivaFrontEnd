cdc93457ed24e7d1327d246124dec4ab4b9f7b31
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-08-15 21:06:19 -0300

    map produtos home e alteracao no cardproduto

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Box, Typography, Button } from "@mui/material";
 import './Home.css';
 import { Paper } from "@material-ui/core";
 import Carrosel from "../../components/carrosel/Carousel";
 import CardProduto from "../../components/cardproduto/CardProduto";
+import { busca } from "../../services/Service";
+import Produto from "../../models/Produto";
+import { useSelector } from "react-redux";
+import { TokenState } from "../../store/tokens/tokensReduce";
 
 
 function Home() {
+
+  const [produtos, setProduto] = useState<Produto[]>([]);
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+    (state) => state.tokens
+  );
+
+  const getProdutos = async () => {
+    //adicionar try catch
+    await busca("/produtos", setProduto, {
+    headers: {
+      'Authorization':  token
+    }
+  });
+}
+
+React.useEffect(() => {
+  getProdutos();
+} , [produtos.length]);
+
   return (
     <>
     
@@ -15,24 +38,22 @@ function Home() {
           <Carrosel/>
         </Grid>
         <Grid item xs={12} className="title-container">
-          <hr /> 
-          <Typography variant="h5" component="h1" className="title-produtos" >
-                Quero levar para casa
+          <hr className="linhaHome" /> 
+          <Typography variant="h5" component="h1" className="title-produtos tituloInicialHome" >
+                Destaques
           </Typography>
-          <hr />
+          <hr className="linhaHome" />
         </Grid>
-        <Grid item xs={3}>
-          <Paper elevation={3} > <CardProduto /> </Paper>
+        {produtos.map(produto => (
+        <Grid item xs={3} key={produto.id}>
+            <CardProduto 
+            nome={produto.nome} 
+            descricao={produto.descricao} 
+            preco={produto.preco}
+            imagem={produto.foto}
+            /> 
         </Grid>
-        <Grid item xs={3}>
-          <Paper elevation={3} > <CardProduto /> </Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper elevation={3} > <CardProduto /> </Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper elevation={3} > <CardProduto /> </Paper>
-        </Grid>
+        ))}
       </Grid>
       
     </>
